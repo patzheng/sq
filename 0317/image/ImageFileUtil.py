60895947dac0f281f60dd746706cf5c05039b95d
pat <597439394@163.com>
2018-03-20 20:18:38 +0800

    线性回归

--- 0317/image/ImageFileUtil.py
+++ 0317/image/ImageFileUtil.py
@@ -1,20 +1,32 @@
 # 如何得到train_x和Train_y
 import os
-from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from PIL import Image
 import numpy as np
+from keras.datasets import mnist
 
 '''
 '''
+
+
+def test():
+    (train_x, train_y), (test_x, test_y) = mnist.load_data()
+    print(train_x.shape)
+    print(train_y.shape)
+
+
 def load_data():
-    data = np.empty((100, 1, 50, 28), dtype="float32")
-    label = np.empty((100,), dtype="uint8")
+    print('load data from image')
     imgs = os.listdir("../data/train/")
+    num = len(imgs)
+    print("num", num)
+    data = np.empty((num, 50, 28), dtype="float32")
+    label = np.empty((num,), dtype="uint8")
+
     i = 0
     for imgFile in imgs:
         img = Image.open("../data/train/" + imgFile).convert('L')
         arr = np.asarray(img, dtype="float32")
-        data[i, :, :, :] = arr
+        data[i, :, :] = arr
         label[i] = int(imgs[i].split('.')[0].split('_')[1])
         i = i + 1
     return data, label
@@ -22,5 +34,10 @@ def load_data():
 
 if __name__ == "__main__":
     data, label = load_data()
-    for i in label:
-        print("label:", i)
+    (train_x, train_y), (test_x, test_y) = mnist.load_data()
+
+    print(label[0])
+    print(train_y[0])
+
+    print(data[0])
+    print(train_x[0])
